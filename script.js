fd94d236a94dd373cb927c5e99305e0a4903a659
Faisal <faisalkhalid98@hotmial.com>
2022-08-25 13:45:38 +0300

    playRound Function Created
    playRound function is created to play a round of the game based on the
    choices of the computer and the player. Then a message is prompted to
    state the winner of the game.

--- script.js
+++ script.js
@@ -10,3 +10,30 @@ function playerSelection() {
     return choice;
 }
 
+function playRound(computer, player) {
+    if (computer === 'rock'){
+        if (player === 'rock') {
+            prompt("It's a tie!");
+        } else if (player === 'paper') {
+            prompt("You Win! Paper beats rock!");
+        } else {
+            prompt("You lose! Rock beats scissors!")
+        }
+    } else if (computer === 'paper') {
+        if (player === 'paper') {
+            prompt("It's a tie!");
+        } else if (player === 'scissors') {
+            prompt("You Win! Scissors beats paper!");
+        } else {
+            prompt("You lose! Paper beats rock!")
+        }
+    } else {
+        if (player === 'scissors') {
+            prompt("It's a tie!");
+        } else if (player === 'rock') {
+            prompt("You Win! Rock beats scissors!");
+        } else {
+            prompt("You lose! Scissors beats paper!")
+        }
+    }
+}
